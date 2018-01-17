e708bc0a8879744d211609e1a02fb63b05adabc8
Anuyta <ryabtseva.anna.92@mail.ru>
2018-01-17 16:19:30 +0300

    task 2. Builder
    fluent interface for registration tax free in shop

---
+++ Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new TaxFreeBuilder(10);
+
+            var taxFree = builder.BuildSurname("Ivanov")
+                .BuildName("Ivan")
+                .BuildCountry("Poland")
+                .BuildCity("Krakov")
+                .BuildDate(new DateTime(2017, 2, 28))
+                .BuildPrice(200).GetTaxFreeForm();
+
+            Console.WriteLine(taxFree);
+            Console.ReadLine();
+        }
+    }
+}
