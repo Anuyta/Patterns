4a995134742106de43eba7e2735a5493b1e19cea
Anuyta <ryabtseva.anna.92@mail.ru>
2018-01-17 16:22:58 +0300

    task 4. Singleton
    Logging service

---
+++ Singleton/LoggerSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class LoggerSingleton
+    {
+        private static readonly LoggerSingleton instance = new LoggerSingleton();
+
+        static LoggerSingleton() //lazy init
+        {
+        }
+
+        private LoggerSingleton()
+        {
+        }
+
+        public static LoggerSingleton GetInstance()
+        {
+            return instance;
+        }
+
+        public void AddToLog(string filePath, string mess)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(mess);
+            }
+        }
+    }
+}
