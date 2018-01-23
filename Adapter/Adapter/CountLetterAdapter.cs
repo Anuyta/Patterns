c69f34d962f4f12fd79a3491aba8de5fade9d200
Anuyta <ryabtseva.anna.92@mail.ru>
2018-01-23 10:53:46 +0300

    task 1. Adapter
    Adapter for class string allow count letters in string

---
+++ Adapter/Adapter/CountLetterAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class CountLetterAdapter : ICountLetter
+    {
+        string str;
+
+        public CountLetterAdapter(string str)
+        {
+            this.str = str;
+        }
+
+        public int CountLetter(char c)
+        {
+            var count = str.Count(chr => chr == c);
+            return count;
+        }
+    }
+}
---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "aaaabcadda  d";
+            var adapt = new CountLetterAdapter(str);
+            char lett1 = 'd';
+            char lett2 = 'a';
+            var count1 = adapt.CountLetter(lett1);
+            var count2 = adapt.CountLetter(lett2);
+            Console.WriteLine("string - {0}", str);
+            Console.WriteLine("letter - {0} - count = {1}", lett1, count1);
+            Console.WriteLine("letter - {0} - count = {1}", lett2, count2);
+            Console.ReadLine();
+        }
+    }
+}
