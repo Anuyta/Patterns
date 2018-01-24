7f5d8afb09b3c42ff9980a9e01deccbfebe09176
Anuyta <ryabtseva.anna.92@mail.ru>
2018-01-24 17:07:23 +0300

    task 3. Observer
    Every 5 sec value of propery ClassA's changed third-party class and every listeneres of ClassA write new value and Datetime.Now in file.

---
+++ BehavioralPatterns/Observer/OtherClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class OtherClass : IObserver<ClassA>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(ClassA a)
+        {
+            using (StreamWriter sw = new StreamWriter("value.txt", true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(String.Format("value = {0}, time - {1}", a.Value, DateTime.Now));
+            }
+        }
+    }
+}
