f768b5cc2e9a0b268f24b416460f8e8a4083f3d8
Anuyta <ryabtseva.anna.92@mail.ru>
2018-01-30 22:19:51 +0300

    task 3. Flyweight

---
+++ Task1/Task3/Element.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace Task3
+{
+    public abstract class Element
+    {
+        public abstract void Drow(Graphics gr, Point p1, Point p2);
+    }
+}
---
+++ Task1/Task3/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Task3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDlg = new System.Windows.Forms.ColorDialog();
+            this.btnChooseColor = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChooseColor
+            // 
+            this.btnChooseColor.Location = new System.Drawing.Point(227, 12);
+            this.btnChooseColor.Name = "btnChooseColor";
+            this.btnChooseColor.Size = new System.Drawing.Size(157, 28);
+            this.btnChooseColor.TabIndex = 3;
+            this.btnChooseColor.Text = "Выбрать цвет";
+            this.btnChooseColor.UseVisualStyleBackColor = true;
+            this.btnChooseColor.Click += new System.EventHandler(this.btnChooseColor_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(447, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 27);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnChooseColor_Click);
+            // 
+            // pbMain
+            // 
+            this.pbMain.BackColor = System.Drawing.Color.White;
+            this.pbMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbMain.Location = new System.Drawing.Point(70, 73);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(809, 385);
+            this.pbMain.TabIndex = 5;
+            this.pbMain.TabStop = false;
+            this.pbMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbMain_MouseDown);
+            this.pbMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbMain_MouseMove);
+            this.pbMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbMain_MouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 512);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnChooseColor);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDlg;
+        private System.Windows.Forms.Button btnChooseColor;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pbMain;
+    }
+}
+
