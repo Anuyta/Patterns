cbc4c0811f4345011ee670fad8cfdbd464b43e2b
Anuyta <ryabtseva.anna.92@mail.ru>
2018-01-30 22:32:38 +0300

    task 5.Observer and Iterator

---
+++ Task1/Task5/Observer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Observer : IObserver<IntegerNumberCollection>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(IntegerNumberCollection value)
+        {
+            Console.WriteLine("I got sorted collection");
+        }
+    }
+}
---
+++ Task1/Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collect = new IntegerNumberCollection();
+            var observer = new Observer();
+            collect.Subscribe(observer);
+
+            collect.AddToCollection(8);
+            collect.AddToCollection(15);
+            collect.AddToCollection(3);
+            collect.AddToCollection(7);
+
+            collect.RemoveFromCollection(2);
+
+            foreach (var item in collect)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
