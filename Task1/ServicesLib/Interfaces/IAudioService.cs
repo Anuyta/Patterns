6bbf5118c76200734b41432aaa2a006518dcf99d
Anuyta <ryabtseva.anna.92@mail.ru>
2018-02-02 16:10:53 +0300

    Task 1. Abstract Factory, Singleton

---
+++ Task1/ServicesLib/Interfaces/IAudioService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLib.Interfaces
+{
+    public interface IAudioService
+    {
+        void Play();
+
+        void Stop();
+
+        AudioInfo GetInfoAboutFile(string fileName);
+    }
+}
